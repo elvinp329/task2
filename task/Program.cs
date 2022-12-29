c4309fd1a9ca2933d77d89eaf728781c08f35f73
Elvinp329 <elvinkj@code.edu.az>
2022-12-29 18:36:20 +0400

    task22

---
+++ task/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            int count = (int)Math.Log10(number) + 1;
+
+            int r;
+            int sum = 0;
+            while (number != 0)
+            {
+                r = number % 10;
+                number = number / 10;
+                sum += r * count;
+                count--;
+                                                 
+             }
+
+                
+            Console.WriteLine(sum);
+            Console.WriteLine("Hello!");
+        }
+    }
+}
